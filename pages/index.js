e3d51b5d1616ce49a727fb19a31e7430641787b1
MarcoTomasRodriguez <marcotomasrodriguez@gmail.com>
2020-02-10 21:50:08 -0300

    Initial setup.

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import '../assets/styles/main.css'
+
+class Index extends React.Component {
+    render() {
+        return (
+            <h1>Index</h1>
+        )
+    }
+}
+
+export default Index
